d6d7f46f308151d28b906d199b9f63986b96f37d
Mike Urbanski <mike@bridgecrew.io>
2020-07-17 14:19:00 -0500

    test bad SG

---
+++ main.tf
@@ -0,0 +1,23 @@
+provider "aws" {
+region = "us-west-2"
+}
+
+resource "aws_security_group" "bar-sg" {
+  name   = "mysg"
+  vpc_id = "vpc-013e69f5146412c46"
+
+  ingress {
+    from_port = 22
+    to_port   = 22
+    protocol  = "tcp"
+    cidr_blocks       = ["0.0.0.0/0"]
+    description = "foo"
+  }
+
+  egress {
+    from_port = 0
+    to_port   = 0
+    protocol  = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
