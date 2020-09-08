62f4244d3b9c6bf667fb76bd69fa4a9919f50daf
mikeurbanski1 <mikeurbanski1@users.noreply.github.com>
2020-09-08 17:39:09 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -24,3 +24,13 @@ resource "aws_s3_bucket" "untagged-bucket" {
 resource "aws_s3_bucket" "bucket2" {
  bucket = "bc-mike-test"
 }
+
+resource "aws_ebs_volume" "ebs1" {
+ bucket = "bc-mike-test"
+ name = "abc"
+}
+
+resource "aws_ebs_volume" "ebs2" {
+ bucket = "bc-mike-test"
+ name = "myname"
+}
